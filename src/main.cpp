06d86e0daa3bb561a7b0487af3474fcd5f00e781
Liam Flaherty <liamjfla@gmail.com>
2023-09-17 00:25:53 +0930

    add parameter parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include <iostream>
 #include "cxxopts.hpp"
 
 int main(int argc, char* argv[])
 {
+    cxxopts::Options options("riscvdb", "RISC V simulator and debugger");
+    options.add_options()
+        ("executable", "The RISC V binary to execute", cxxopts::value<std::string>())
+        ("h,help", "Print usage");
+    options.parse_positional({"executable"});
+    options.positional_help("riscv_binary_file");
+
+    cxxopts::ParseResult result;
+    try
+    {
+        result = options.parse(argc, argv);
+    }
+    catch (cxxopts::exceptions::exception& ex)
+    {
+        std::cerr << "error: " << ex.what() << std::endl;
+        return -1;
+    }
+
+    if (result.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    if (result.count("executable"))
+    {
+        std::string binFile = result["executable"].as<std::string>();
+        std::cout << "Loading bin file " << binFile << std::endl;
+    }
+
     return 0;
 }
