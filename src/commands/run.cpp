eb3a90292d7212bfe3e74fc81c7777e8eb555971
Liam Flaherty <liamjfla@gmail.com>
2024-05-30 22:09:21 +0930

    improve run command; allow run command to reset program

--- src/commands/run.cpp
+++ src/commands/run.cpp
@@ -2,8 +2,13 @@
 
 #include <iostream>
 #include <csignal>
+#include <cstring>
 
 namespace riscvdb {
+
+const std::string CmdRun::MSG_USAGE =
+"Usage: run [number of instructions]\n"
+"Set number of instructions to 0 or omit to run indefinitely";
 
 CmdRun::CmdRun(SimHost& simHost)
 : m_simHost(simHost)
@@ -15,12 +20,15 @@ void CmdRun::sigint_handler(int signum)
 {
   if (signum == SIGINT)
   {
-    // user hit Ctrl-C before program finished
-    std::cout << std::endl;
-    std::cout << "user interrupted" << std::endl;
+    if (m_simHost.GetState() == SimHost::RUNNING)
+    {
+      // user hit Ctrl-C before program finished
+      std::cout << std::endl;
+      std::cout << "user interrupted" << std::endl;
 
-    // this will eventually change the state
-    m_simHost.Pause();
+      // this will eventually change the state
+      m_simHost.Pause();
+    }
   }
 }
 
@@ -35,6 +43,12 @@ ConsoleCommand::CmdRetType CmdRun::run(std::vector<std::string>& args)
       break;
 
     case 2:
+      if (args[1] == "-h" || args[1] == "--help")
+      {
+        std::cerr << MSG_USAGE << std::endl;
+        return CmdRetType_OK;
+      }
+
       try
       {
         numInstructions = std::stoul(args[1]);
@@ -48,17 +62,35 @@ ConsoleCommand::CmdRetType CmdRun::run(std::vector<std::string>& args)
       break;
 
     default:
-      std::cerr << "Usage: run [number of instructions]" << std::endl;
+      std::cerr << MSG_USAGE << std::endl;
       return CmdRetType_ERROR;
   }
 
-  if (m_simHost.GetState() == SimHost::RUNNING ||
-      m_simHost.GetState() == SimHost::PAUSED)
+  // if program is already running, then ask if we want to restart
+  if (m_simHost.GetState() == SimHost::PAUSED)
   {
-    std::cerr << "target is already running" << std::endl;
-    return CmdRetType_ERROR;
+    char select = 0;
+    while (!(std::tolower(select) == 'y' || std::tolower(select) == 'n'))
+    {
+      // TODO: if you ctrl C here, it closes the console...
+      std::cout << "Target is already running. Would you like to restart? (y/n) ";
+      std::cin >> select;
+    }
+
+    if (std::tolower(select) == 'n')
+    {
+      // ok, just bail then
+      return CmdRetType_OK;
+    }
+    else
+    {
+      std::cout << "resetting machine" << std::endl;
+      // restart target and run
+      m_simHost.ResetSim();
+    }
   }
 
+  // begin running
   try
   {
     m_simHost.Run(numInstructions);
@@ -72,6 +104,7 @@ ConsoleCommand::CmdRetType CmdRun::run(std::vector<std::string>& args)
   // Register a SIGINT handler for this class
   SigIntHandler sigint(std::bind(&CmdRun::sigint_handler, this, std::placeholders::_1));
 
+  // and wait while running
   while (m_simHost.GetState() == SimHost::RUNNING)
   {
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -82,6 +115,16 @@ ConsoleCommand::CmdRetType CmdRun::run(std::vector<std::string>& args)
     std::cout << "target paused" << std::endl;
     std::cout << "PC = 0x";
     std::cout << std::hex << m_simHost.Processor().GetPC() << std::endl;
+    std::cout << std::dec << m_simHost.Processor().GetInstructionCount();
+    std::cout << " instructions executed" << std::endl;
+  }
+  else if (m_simHost.GetState() == SimHost::TERMINATED)
+  {
+    std::cout << "target terminated" << std::endl;
+    std::cout << "PC = 0x";
+    std::cout << std::hex << m_simHost.Processor().GetPC() << std::endl;
+    std::cout << std::dec << m_simHost.Processor().GetInstructionCount();
+    std::cout << " instructions executed" << std::endl;
   }
 
   return CmdRetType_OK;
