4e3c7cfe8c3d5f3a266d04d814121c2b61eb6d34
Liam Flaherty <liamjfla@gmail.com>
2024-05-31 22:33:24 +0930

    load symbols

--- src/fileloader.cpp
+++ src/fileloader.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <cstring>
 #include <algorithm>
+#include <sstream>
 #include "elf.h"
 
 namespace riscvdb {
@@ -117,28 +118,39 @@ void ElfFileLoader::LoadHeader()
 void ElfFileLoader::LoadProgramHeaders(MemoryMap& mem)
 {
     std::cout << "Found program headers:" << std::endl;
+    std::cout << "  ";
+    std::cout << std::setw(4) << std::setfill(' ') << std::left << "Num";
+    std::cout << std::setw(8) << std::setfill(' ') << std::left << "Type";
+    std::cout << std::setw(11) << std::setfill(' ') << std::left << "VirtAddr";
+    std::cout << std::setw(11) << std::setfill(' ') << std::left << "MemSiz";
+    std::cout << std::endl;
 
     MemoryMap::AddrType loadedSize = 0;
 
     Elf32_Off offset = m_header.e_phoff;
     for (Elf32_Half i = 0; i < m_header.e_phnum; ++i)
     {
-        std::cout << i << " ";
+        std::cout << "  ";
+        std::cout << std::setw(4) << std::setfill(' ') << std::left << i;
 
         Elf32_Phdr progHdr;
         std::memcpy(&progHdr, m_filebytes.data() + offset,
                     sizeof(progHdr));
 
-        offset += sizeof(progHdr);
+        offset += m_header.e_phentsize;
 
         if (progHdr.p_type == PT_LOAD)
         {
-            std::cout << "LOAD  ";
-            std::cout << "  VirtAddr = 0x";
-            std::cout << std::hex << std::setw(8) << std::left << std::setfill(' ');
+            std::cout << std::setw(8) << std::setfill(' ') << std::left;
+            std::cout << "LOAD";
+
+            std::cout << "0x";
+            std::cout << std::hex << std::setw(8) << std::right << std::setfill('0');
             std::cout << progHdr.p_paddr;
-            std::cout << "  MemSize = 0x";
-            std::cout << std::hex << std::setw(8) << std::left << std::setfill(' ');
+            std::cout << " ";
+
+            std::cout << "0x";
+            std::cout << std::hex << std::setw(8) << std::right << std::setfill('0');
             std::cout << progHdr.p_memsz;
             std::cout << std::endl;
 
@@ -158,9 +170,8 @@ void ElfFileLoader::LoadProgramHeaders(MemoryMap& mem)
         }
     }
 
-    std::cout << std::endl;
     std::cout << std::dec;
-    std::cout << "Loaded " << loadedSize << " B into memory" << std::endl;
+    std::cout << "Loaded " << loadedSize << " bytes into memory" << std::endl;
     std::cout << std::endl;
 }
 
@@ -182,47 +193,56 @@ void ElfFileLoader::LoadSymbols(SimHost& simHost)
 
 
     std::cout << "Found sections:" << std::endl;
+    std::cout << "  ";
+    std::cout << std::setw(4) << std::setfill(' ') << std::left << "Num";
+    std::cout << std::setw(18) << std::setfill(' ') << std::left << "Name";
+    std::cout << std::setw(18) << std::setfill(' ') << std::left << "Type";
+    std::cout << std::setw(11) << std::setfill(' ') << std::left << "Off";
+    std::cout << std::endl;
 
     Elf32_Off offset = m_header.e_shoff;
     for (Elf32_Half i = 0; i < m_header.e_shnum; ++i)
     {
-        std::cout << i << " ";
+        std::cout << "  ";
+        std::cout << std::setw(4) << std::setfill(' ') << std::left << i;
 
         Elf32_Shdr sectionHdr;
         std::memcpy(&sectionHdr, m_filebytes.data() + offset,
                     sizeof(sectionHdr));
 
-        offset += sizeof(sectionHdr);
+        offset += m_header.e_shentsize;
 
         // get name:
+        std::stringstream name_ss;
         Elf32_Off nameOffset = shStrTable.sh_offset + sectionHdr.sh_name;
         std::byte c;
         while ((c = m_filebytes[nameOffset++]) != std::byte{'\0'})
         {
-            std::cout << static_cast<char>(c);
+            name_ss << static_cast<char>(c);
         }
+        std::cout << std::setw(18) << std::setfill(' ') << std::left;
+        std::cout << name_ss.str();
 
-        std::cout << "\t";
-
+        std::cout << std::setw(18) << std::setfill(' ') << std::left;
         switch (sectionHdr.sh_type)
         {
             case SHT_STRTAB:
             {
-                std::cout << "STRTAB";
-
                 if (i == m_header.e_shstrndx)
                 {
-                    std::cout << "  (shstrtab)";
-                    break;
+                    std::cout << "STRTAB (shstrtab)";
                 }
-
-                if (strTableFound)
+                else
                 {
-                    throw std::runtime_error("multiple string tables in ELF");
-                }
+                    std::cout << "STRTAB";
+                    if (strTableFound)
+                    {
+                        throw std::runtime_error("multiple string tables in ELF");
+                    }
 
-                strTableFound = true;
-                strTableHdr = sectionHdr;
+                    strTableFound = true;
+                    strTableHdr = sectionHdr;
+                }
                 break;
             }
 
@@ -241,11 +261,12 @@ void ElfFileLoader::LoadSymbols(SimHost& simHost)
             }
 
             default:
-                std::cout << "(unknown: " << sectionHdr.sh_type << ")";
+                std::cout << "(other)";
         }
 
-        std::cout << std::hex;
-        std::cout << "\toffset " << sectionHdr.sh_offset;
+        std::cout << "0x";
+        std::cout << std::setw(8) << std::setfill('0') << std::right;
+        std::cout << sectionHdr.sh_offset;
         std::cout << std::endl;
     }
 
@@ -254,24 +275,113 @@ void ElfFileLoader::LoadSymbols(SimHost& simHost)
     if (symTableFound && strTableFound)
     {
         std::cout << std::endl;
-        std::cout << "Found symbols:" << std::endl;
+        std::cout << "Found symbols: " << std::endl;
+        std::cout << "  ";
+        std::cout << std::setw(4) << std::setfill(' ') << std::left << "Num";
+        std::cout << std::setw(11) << std::setfill(' ') << std::left << "Value";
+        std::cout << std::setw(8) << std::setfill(' ') << std::left << "Type";
+        std::cout << std::setw(11) << std::setfill(' ') << std::left << "Bind";
+        std::cout << std::setw(11) << std::setfill(' ') << std::left << "Name";
+        std::cout << std::endl;
+
         // read symbol table
+        Elf32_Word numSymbols = symTableHdr.sh_size / symTableHdr.sh_entsize;
         Elf32_Off symTableOffset = symTableHdr.sh_offset;
-        for (Elf32_Word i = 0; i < symTableHdr.sh_entsize; ++i)
+        for (Elf32_Word i = 0; i < numSymbols; ++i)
         {
-            std::cout << std::dec << i << " ";
+            std::cout << "  ";
+            std::cout << std::setw(4) << std::setfill(' ') << std::left;
+            std::cout << std::dec;
+            std::cout << i;
 
             Elf32_Sym symbol;
             std::memcpy(&symbol, m_filebytes.data() + symTableOffset,
                         sizeof(symbol));
 
-            symTableOffset += sizeof(symbol);
+            symTableOffset += symTableHdr.sh_entsize;
 
-            // TODO decode symbol and store in simHost
-            (void)simHost;
+            // decode symbol to store in simHost
+            SimHost::Symbol symbolLoad;
+
+            // symbol value:
+            std::cout << "0x";
+            std::cout << std::setw(8) << std::setfill('0') << std::right;
+            std::cout << std::hex << symbol.st_value;
+            std::cout << " ";
+            symbolLoad.addr = symbol.st_value;
+
+            std::cout << std::setw(8) << std::setfill(' ') << std::left;
+            switch (ELF32_ST_BIND(symbol.st_info))
+            {
+                case STB_LOCAL:
+                    std::cout << "LOCAL";
+                    break;
+                case STB_GLOBAL:
+                    std::cout << "GLOBAL";
+                    break;
+                case STB_WEAK:
+                    std::cout << "WEAK";
+                    break;
+                default:
+                    std::cout << "unknown";
+            }
+
+            std::cout << std::setw(11) << std::setfill(' ') << std::left;
+            switch (ELF32_ST_TYPE(symbol.st_info))
+            {
+                case STT_NOTYPE:
+                    std::cout << "NOTYPE";
+                    symbolLoad.type = SimHost::SymbolType::NOTYPE;
+                    break;
+                case STT_OBJECT:
+                    std::cout << "OBJECT";
+                    symbolLoad.type = SimHost::SymbolType::OBJECT;
+                    break;
+                case STT_FUNC:
+                    std::cout << "FUNC";
+                    symbolLoad.type = SimHost::SymbolType::FUNC;
+                    break;
+                case STT_SECTION:
+                    std::cout << "SECTION";
+                    symbolLoad.type = SimHost::SymbolType::SECTION;
+                    break;
+                case STT_COMMON:
+                    std::cout << "COMMON";
+                    symbolLoad.type = SimHost::SymbolType::COMMON;
+                    break;
+                case STT_TLS:
+                    std::cout << "TLS";
+                    symbolLoad.type = SimHost::SymbolType::TLS;
+                    break;
+                default:
+                    std::cout << "unknown";
+                    symbolLoad.type = SimHost::SymbolType::UNKNOWN;
+            }
+
+            // symbol name:
+            std::stringstream name_ss;
+            Elf32_Off nameOffset = strTableHdr.sh_offset + symbol.st_name;
+            std::byte c;
+            while ((c = m_filebytes[nameOffset++]) != std::byte{'\0'})
+            {
+                name_ss << static_cast<char>(c);
+            }
+
+            std::cout << std::setw(11) << std::setfill(' ') << std::left;
+            std::cout << name_ss.str();
+
+            // insert into sim's symbol table
+            if (!name_ss.str().empty())
+            {
+                simHost.SymbolMap().insert(std::make_pair(name_ss.str(), symbolLoad));
+            }
 
             std::cout << std::endl;
         }
+
+        std::cout << std::dec;
+        std::cout << "Loaded " << simHost.SymbolMap().size();
+        std::cout << " symbols" << std::endl;
     }
 }
 
